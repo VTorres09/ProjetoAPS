d90dbaf74b8ca2fe91590e5723a07c2764906f20
Gustavo Chaves Galdino de Moraes <gcgm@ug3c43.windows.cin.ufpe.br>
2022-08-10 13:18:18 -0300

    Adding html template engine

--- Entrega 2/Implementacao/src/index.ts
+++ Entrega 2/Implementacao/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 
+let path = require('path');
+
 const app = express()
 
 const port = 3000
 
+app.set('view engine', 'ejs')
+
+app.set('views',  path.join(__dirname, ".", "views"))
+
 app.get("/", (req, res) => {
-    res.send("Hello, world!")
+    res.render('home')
 })
 
 app.listen(port, () => {
