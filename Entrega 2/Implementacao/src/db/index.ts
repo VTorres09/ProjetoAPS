28fdf017fe5d01ede7bb61eaf07df1725a792262
daniel <dap5@cin.ufpe.br>
2022-08-11 18:34:32 -0300

    adding postgres databse connection and updating usuarioBDR

---
+++ Entrega 2/Implementacao/src/db/index.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: "iineeugr",
+  host: "motty.db.elephantsql.com",
+  port: 5432,
+  database: "iineeugr",
+  password: "Fp1_8Y1HQsFaJlblMdsx3MtWk5VnjcUn",
+  // ssl: true,
+});
+
+export default pool;
--- Entrega 2/Implementacao/src/models/repositorio/UsuarioBDR.ts
+++ Entrega 2/Implementacao/src/models/repositorio/UsuarioBDR.ts
@@ -1,8 +1,12 @@
+import pool from "../../db/";
+
 class UsuarioBDR {
     constructor() {}
-
-    inserirUsuario(cpf: string, nome: string) {
+    async inserirUsuario(cpf: string, nome: string) {
         console.log(cpf, nome)
+        const query = `INSERT INTO users (cpf, password) 
+            VALUES ($1, $2) RETURNING cpf`;
+        await pool.query(query, [cpf, nome])
     }
 }
 
