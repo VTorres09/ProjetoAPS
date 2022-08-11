b564ed97ca51398b6f6dd7d482d609735adaf1d1
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2022-08-11 14:42:00 -0300

    adding css and login view

--- Entrega 2/Implementacao/src/index.ts
+++ Entrega 2/Implementacao/src/index.ts
@@ -8,10 +8,12 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
+app.use(express.static(__dirname + '/public/'));
+
 app.set('views',  path.join(__dirname, ".", "views"))
 
 app.get("/", (req, res) => {
-    res.render('home')
+    res.render('LoginView')
 })
 
 app.listen(port, () => {
